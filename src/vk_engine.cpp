b63d0e8f637f158d7a87366667cdee8b109cc08b
retromeister <43223593+retro-git@users.noreply.github.com>
2025-10-04 16:13:27 +0800

    comments

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -227,9 +227,9 @@ void VulkanEngine::draw()
 
 	VkCommandBufferSubmitInfo cmdinfo = vkinit::command_buffer_submit_info(cmd);	
 	
-    // Wait for _swapchainSemaphore to be signaled before starting the commands submitted via vkQueueSubmit2
+    // Block specifically the COLOR_ATTACHMENT_OUTPUT pipeline stage until _swapchainSemaphore is signaled
 	VkSemaphoreSubmitInfo waitInfo = vkinit::semaphore_submit_info(VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT_KHR, get_current_frame()._swapchainSemaphore);
-    // Signal _renderSemaphore when the commands submitted via vkQueueSubmit2 have finished executing
+    // Signal _renderSemaphore when all graphics pipeline stages have finished within the graphics queue
 	VkSemaphoreSubmitInfo signalInfo = vkinit::semaphore_submit_info(VK_PIPELINE_STAGE_2_ALL_GRAPHICS_BIT, get_current_frame()._renderSemaphore);	
 	 
 	VkSubmitInfo2 submit = vkinit::submit_info(&cmdinfo,&signalInfo,&waitInfo);	
